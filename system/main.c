506c5e1cdecd3bda67e679288f58fbde310f826c
Madhav Agrawal <Madhav Agrawal>
2016-11-27 20:53:59 -0500

    readme

--- system/main.c
+++ system/main.c
@@ -18,14 +18,22 @@ uint16  remport;        /* remote sender's UDP port */
 int32   slot;
 uint32 localip;
 
+
+/* UDP message struture*/
 struct msg_struct{
-    uint32 bb_id;
-    uint32 msg_id;
-    uint32 dev_id;
-    uint32 status;
+    uint32 bb_id;  /* ID of the beagle bone present in the XML file*/
+    uint32 msg_id;   /* Message ID for each message sent from UDP connection*/
+    uint32 dev_id;   /*Device ID  for the device connected with the BB mentioend in the XML file*/
+    uint32 status;      /*Actual message or action for the device*/
 };
 
-
+/**************************************************************************
+function name:  cleardata
+description:    Clear all the Msg_struct data 
+input:          pointer to Message structure
+                
+author:         Madhav Agrawal   
+**************************************************************************/
 void cleardata(struct msg_struct *msg){
     msg->bb_id = -1;
     msg->msg_id =  -1;
@@ -34,6 +42,14 @@ void cleardata(struct msg_struct *msg){
 
 }
 
+
+/**************************************************************************
+function name:  gettemp
+description:    get the temperature from the adc 
+input:          void
+return:         return the unsigned int of the temperation          
+author:         Madhav Agrawal   
+**************************************************************************/
 uint32 gettemp(){
 
     uint32 data[4];
@@ -50,6 +66,18 @@ uint32 gettemp(){
 
 
 
+/**************************************************************************
+function name:  BB_init
+description:    When Xinu boots first main will Initialize the UDP connection with the Edge server.The main process will wait 
+for a Init packet from the edge server INIT status  = 200 with the BBB id should be set for particular board.
+So if there are many BBB board are connected with the network each board is waiting for there INIT packet with BBB ID.
+As in start Edge doesn't know IP address of the BBB, Edge will just send the Broadcast INIT packet for each BBB
+with the BB ID set for each board. Once the BB reaceived a INIT packet with its BBID, it will ECHO the packet as a ack  
+to the Edge and stored the IP and port of the Edge server and Initialized the BBB board  otherwise just ignore the 
+packet. Once the BB is initialized it will create two process UDP server and UDP client. 
+input:          void          
+author:         Madhav Agrawal   
+**************************************************************************/
 void BB_init(){
     struct msg_struct msg;
     int32   retval;
@@ -75,15 +103,7 @@ void BB_init(){
     kprintf("initize BBB.........\n");
     while(TRUE) {
         cleardata(&msg);
-       /* msg.dev_id = 1;
-        msg.bb_id = BBID;
-        msg.status = BBB_INIT;
-        msg.msg_id++;
-        retval = udp_sendto(slot,0xC0A86402, 3333, (char*)&msg,sizeof(struct msg_struct));
-            if (retval == SYSERR) {
-                fprintf(stderr, ": udp_sendto failed\n");
-        }
-        continue;*/
+
         retval = udp_recvaddr(slot, &remip, &remport,(char*)&msg,sizeof(struct msg_struct ), 200000);
         if (retval == TIMEOUT) {
             continue;
@@ -107,6 +127,17 @@ void BB_init(){
 
 }
 
+
+/**************************************************************************
+function name:  udpclient process
+description:    UDP client process will send the INPUT data to EDGE server in some interval or based on INPUT interupt.
+If a temperature sensor is connected with the BB it will provid the temperature information to a edge in every 
+1 sec. so that BB can save power and remain idle. If a INPUT button or digital sensor is connected with the BBB 
+BB will triggered IRQ when a rising or falling edge is detected on the device PIN. once the iterrupt is received BB will 
+read the current status of the Input pin and send the status to the Edge server using UDP connection.
+input:          void         
+author:         Madhav Agrawal   
+**************************************************************************/
  process udpclient()
  {
 
@@ -157,6 +188,16 @@ void BB_init(){
 
 
 
+/**************************************************************************
+function name:  udpserver process
+description:    UDP server process will continuously listen for packet. If a packet is reacived with the BB id  and the Device ID 
+if will perform the Operation. For performing any action on any Device Edge has to set the device ID , BB id and 
+Msg ID and the Status for the device. Every device connected to BB has a device id which is mentioned in the XML .
+every message has its ID. and status the action which has to be performed on the device. E.g for a GPIO device 
+like LED status 1 will turn on  the LED and 0 will turn off the LED. 
+input:          void         
+author:         Madhav Agrawal   
+**************************************************************************/
 process udpserver() {
 	kprintf("udp server starts ");
 	struct msg_struct msg;
@@ -229,6 +270,12 @@ process udpserver() {
 }
 
 
+/**************************************************************************
+function name:  main
+description:    main function first Initialize the BBB then create two process one UDP client and One udp server.
+input:          void        
+author:         Madhav Agrawal   
+**************************************************************************/
 process	main(void)
 {
  
@@ -244,44 +291,4 @@ process	main(void)
 	return OK;
 }
 
-/*process main(void)
-{
-
-    recvclr();
-    
-
-
-
-    kprintf("ADC init complete\n");
-
-    putc(LED,1);
-
-    kprintf("Start main function\r\n");
-    char data[4];
-    unsigned int integer = 0;
-    unsigned int decimal = 0;
-    float voltage;
-    while(1)
-    {
-        read(TEMP,(char*)data,4);
-        unsigned int vol = *(unsigned int*)data;
-        voltage = (vol*1.8/4095)*1000;
-
-        integer = (unsigned int)voltage;
-        decimal = (unsigned int)((voltage - integer)*10);
-        kprintf("voltage = %d.%d  mv\r\n",integer,decimal);
-        sleep(1);
-    }
-
-    //getc(TEMP);
-    while(1){
-        char value = getc(INPUT);
-            kprintf("value = %u\n", value);
-        //kprintf("data value = %f",((float)analogRead() / 4095.0f) * 1.8f);
-    }
-
-    
-
-    return OK;
-}*/
 
